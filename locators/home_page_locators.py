1196ed181859ba0d4a94d9d571c49cebdcf9148c
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-11-09 14:14:45 +0100

    Update home_page_locators.py

--- locators/home_page_locators.py
+++ locators/home_page_locators.py
@@ -3,21 +3,21 @@ from selenium.webdriver.common.by import By
 
 class HomePageHeaderLocators:
     """Верхняя часть главной страницы"""
-    logo_yandex = (By.XPATH, ".//a[contains(@class, 'LogoYandex')]")  # Изменено: contains для гибкости
-    logo_scooter = (By.XPATH, ".//a[contains(@class, 'LogoScooter')]")  # Изменено: contains для гибкости
-    order_button = (By.XPATH, "//div[@class='Header_Buttons__1FXDj']/button[text()='Заказать']")  # Уточнено: поиск кнопки "Заказать" внутри div
-    order_status_button = (By.XPATH, ".//button[contains(text(), 'Статус заказа')]")  # Изменено: contains для большей устойчивости
-    track_field = (By.XPATH, ".//input[@placeholder='Введите номер заказа']")  # Более семантичный локатор
-    view_button = (By.XPATH, ".//button[text()='Посмотреть']")  # Более семантичный локатор
-    header_page_title = (By.XPATH, ".//div[contains(text(), 'Учебный тренажер')]")  # Изменено: contains для устойчивости
+    logo_yandex = (By.XPATH, ".//a[contains(@class, 'LogoYandex')]")  
+    logo_scooter = (By.XPATH, ".//a[contains(@class, 'LogoScooter')]")  
+    order_button = (By.XPATH, "//div[@class='Header_Buttons__1FXDj']/button[text()='Заказать']")  
+    order_status_button = (By.XPATH, ".//button[contains(text(), 'Статус заказа')]")  
+    track_field = (By.XPATH, ".//input[@placeholder='Введите номер заказа']")  
+    view_button = (By.XPATH, ".//button[text()='Посмотреть']")  
+    header_page_title = (By.XPATH, ".//div[contains(text(), 'Учебный тренажер')]") 
 
 
 class HomePageLocators:
     """Элементы главной страницы"""
-    home_page_title = (By.XPATH, ".//div[contains(@class, 'Header__iJKdX')]")  # Изменено: contains для гибкости
-    order_button = (By.XPATH, "//div[@class='Home_Buttons__1JtPq']/button[text()='Заказать']")  # Уточнено: поиск кнопки "Заказать" внутри div
+    home_page_title = (By.XPATH, ".//div[contains(@class, 'Header__iJKdX')]")  
+    order_button = (By.XPATH, "//div[@class='Home_Buttons__1JtPq']/button[text()='Заказать']") 
     accept_cookies_button = (By.ID, "rcc-confirm-button")
-    questions_title = (By.XPATH, "//div[contains(text(), 'Вопросы о важном')]")  # Изменено: contains для устойчивости
+    questions_title = (By.XPATH, "//div[contains(text(), 'Вопросы о важном')]")  
 
     # Локаторы кнопок вопросов
     questions = [
