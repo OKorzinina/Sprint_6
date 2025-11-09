5c120471a5f89ea87b91187c1dedea77651a989c
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-11-09 14:23:00 +0100

    Update dzen_page.py

--- pages/dzen_page.py
+++ pages/dzen_page.py
@@ -1,1 +1,12 @@
+from pages.base_page import BasePage
+from locators.dzen_page_locators import DzenPageLocators
+import allure
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+class DzenPage(BasePage):
+
+    @allure.step('Проверка отображения кнопки "Главная" на странице DZEN')
+    def check_element_main_button(self):
+        return self.check_element(DzenPageLocators.main_button_dzen)
 
