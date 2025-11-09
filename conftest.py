38ac42168fa4317e344975f783fb0ca8546677c1
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-11-09 22:01:44 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,19 +1,33 @@
 import allure
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions 
+from webdriver_manager.firefox import GeckoDriverManager
 
 from helps.data import Urls
 
 
-@allure.step('Открытие браузера / переход на страницу сервиса / закрытие браузера')
-@pytest.fixture
+@pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Firefox()
-    driver.get(Urls.QA_SCOOTER_URL)
+    geckodriver_path = GeckoDriverManager().install()
+    service = FirefoxService(executable_path=geckodriver_path)
+
+    firefox_options = FirefoxOptions()
+    firefox_binary_location = r"C:\Users\Olga\AppData\Local\Mozilla Firefox\firefox.exe" 
+    firefox_options.binary_location = firefox_binary_location
+    driver = webdriver.Firefox(service=service, options=firefox_options)
+    driver.set_window_size(1920, 1080)
+    with allure.step('Переход на страницу сервиса'):
+        driver.get(Urls.QA_SCOOTER_URL)
     yield driver
-    driver.quit()
+    with allure.step('Закрытие браузера'):
+        driver.quit()
 
 
-# Корректное отображение аргументов в параметризированном тесте
 def pytest_make_parametrize_id(val):
+    if isinstance(val, tuple) and len(val) == 3:
+        if 'accordion__heading' in val[0][1]:
+            return f"Question {val[0][1].split('-')[-1]}"
+        return repr(val)
     return repr(val)
