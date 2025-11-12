ed75dad31a9855d59eec8a2cb606c3b2e09e0534
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-11-12 11:32:40 +0100

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+import allure 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -7,36 +8,37 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
-    # Получить текущий URL
+    @allure.step("Получить текущий URL") # Добавлена аннотация allure.step
     def get_current_url(self):
         return self.driver.current_url
 
-    # Ожидание отображения локатора
+    @allure.step("Ожидание отображения элемента по локатору {locator}") # Добавлена аннотация allure.step
     def find_and_wait_locator(self, locator):
         return WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located(locator)
             )
 
-    # Клик по кнопке
+    @allure.step("Кликнуть по кнопке/элементу по локатору {locator}") # Добавлена аннотация allure.step
     def click_button(self, locator):
         self.find_and_wait_locator(locator).click()
 
-    # Заполнение формы
+    @allure.step("Заполнить поле по локатору {locator} текстом: '{text}'") # Добавлена аннотация allure.step
     def send_keys_to_field(self, locator, text):
         self.find_and_wait_locator(locator).send_keys(text)
 
-    # Получить текст элемента
+    @allure.step("Получить текст элемента по локатору {locator}") # Добавлена аннотация allure.step
     def get_text_locator(self, locator):
         return self.find_and_wait_locator(locator).text
 
-    # Скролл к нужному элементу
+    @allure.step("Прокрутить страницу до элемента по локатору {locator}") # Добавлена аннотация allure.step
     def scroll_to_locator(self, locator):
         element = self.find_and_wait_locator(locator)
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
-    # Переход на новую вкладку браузера
+    @allure.step("Переключиться на новую вкладку браузера") # Добавлена аннотация allure.step
     def go_to_new_tab(self):
         self.driver.switch_to.window(self.driver.window_handles[1])
 
+    @allure.step("Проверить отображение элемента по локатору {locator}") # Добавлена аннотация allure.step
     def check_element(self, locator):
         return self.find_and_wait_locator(locator).is_displayed()
